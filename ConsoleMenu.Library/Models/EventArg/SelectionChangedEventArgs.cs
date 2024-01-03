7af86839bd7b848c379b1efd61280e2f35104876
MikyGus <mikygus@gmail.com>
2024-01-03 22:21:19 +0100

    Remove SelectionManager

--- ConsoleMenu.Library/Models/EventArg/SelectionChangedEventArgs.cs
+++
@@ -1,7 +0,0 @@
-﻿using ConsoleMenu.Library.Managers;
-
-namespace ConsoleMenu.Library.Models.EventArg;
-internal class SelectionChangedEventArgs : ISenderEvent<ISelectionManager>
-{
-    public required ISelectionManager Sender { get; set; }
-}
